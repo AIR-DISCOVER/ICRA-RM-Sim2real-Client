1bf523953138af4512a4b764ac5f85c6af4ebbf7
George Tian <tianbeiwen88@gmail.com>
2022-10-21 13:25:07 +0800

    update client base

---
+++ proxy.sh
@@ -0,0 +1,7 @@
+PROXY=http://localhost:50000
+export HTTP_PROXY=$PROXY
+export HTTPS_PROXY=$PROXY
+export ALL_PROXY=$PROXY
+export http_proxy=$PROXY
+export https_proxy=$PROXY
+export all_proxy=$PROXY
